cb01e38a61ec923da3dc67e53d55941203d7857e
Brais Gonzalez <braisgmendu@gmail.com>
2025-02-11 21:23:58 +0100

    agenda_telf implemented

--- agenda_telf/src/App.jsx
+++ agenda_telf/src/App.jsx
@@ -1,36 +1,68 @@
 import { useState } from 'react'
-import Number from './components/Number'
+import Person from './components/Person'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+
   const addPerson = (event) =>{
     event.preventDefault()
     const personsObject ={
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    setPersons(persons.concat(personsObject))
+    const nombreExiste = persons.some(person => person.name === newName)
+    const numeroExiste = persons.some(person => person.number === newNumber)
+    if(nombreExiste){
+      alert(`El nombre "${newName}" ya existe en la agenda"`)
+    }else if(numeroExiste){
+      alert(`El número "${newNumber}" ya existe en la agenda"`)
+    }
+    else{
+      setPersons(persons.concat(personsObject))
+    }
+    setNewName('')
+      setNewNumber('')
   }
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson} >
-        <div>
-          name: <input value={newName} onChange={handlePersonChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+        <Filter value={searchTerm} onChange={handleSearchChange} />
+      <div> 
+      <h2>add Person</h2>
+        
+        <PersonForm addPerson={addPerson} handleNameChange={handleNameChange} 
+        newName={newName} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      </div>
+     <div>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <Number key={person.name} number={person} />
-      )}
+        {filteredPersons.map(person => 
+          <Person key={person.id} person={person}  />
+        )}
+     </div>
+      
     </div>
   )
 }
--- agenda_telf/src/components/Number.jsx
+++
@@ -1,9 +0,0 @@
-const Number = ({number}) =>{
-    return (
-        <div>
-            <p>{number.name}</p>
-        </div>
-    )
-}
-
-export default Number
